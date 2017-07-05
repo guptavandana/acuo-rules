e88eaa35f4acc980db013ff96a3e83540dadc293
Hicham Medkouri <hicham.medkouri@gmail.com>
2017-07-05 22:49:14 +0100

    OW-695 added a very eligible asset simple rule

---
+++ src/main/java/com/acuo/rules/assets/Eligible.groovy
@@ -0,0 +1,9 @@
+package com.acuo.rules.assets
+
+import lombok.Data
+
+@Data
+class Eligible {
+
+    boolean isEligible = false
+}
---
+++ src/test/groovy/com/acuo/rules/assets/AssetSpec.groovy
@@ -0,0 +1,53 @@
+package com.acuo.rules.assets
+
+import com.acuo.common.model.assets.Assets
+import org.kie.api.KieServices
+import org.kie.api.runtime.KieSession
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import spock.lang.Specification
+
+class AssetSpec extends Specification {
+
+    String ksessionName = "AssetsKS"
+
+    KieSession ksession
+    Logger ruleLogger
+
+    def setup() {
+        def ks = KieServices.Factory.get()
+        def kContainer = ks.getKieClasspathContainer()
+        ksession = kContainer.newKieSession(ksessionName)
+
+        ruleLogger = LoggerFactory.getLogger(ksessionName)
+        ksession.setGlobal("log", ruleLogger)
+    }
+
+    def "should AA assets be eligible"() {
+        when: "add an AA asset"
+        def asset = new Assets(fitchRating: "AA", assetId: "a1")
+        def eligible = new Eligible()
+        ksession.insert(asset)
+        ksession.insert(eligible)
+
+        and: "we fire all rules"
+        ksession.fireAllRules()
+
+        then: "we get eligibility set to true"
+        eligible.isEligible
+    }
+
+    def "should none AA assets not be eligible"() {
+        when: "add a none AA asset"
+        def asset = new Assets(fitchRating: "AB", assetId: "a2")
+        def eligible = new Eligible()
+        ksession.insert(asset)
+        ksession.insert(eligible)
+
+        and: "we fire all rules"
+        ksession.fireAllRules()
+
+        then: "we get eligibility set to true"
+        !eligible.isEligible
+    }
+}
