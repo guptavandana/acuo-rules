c80d2e1dd4e2d7d8378a6a843358b68440443b85
Hicham Medkouri <hicham.medkouri@gmail.com>
2017-07-27 10:42:49 +0100

    OW-722 fixed a NPE in the rule due to logger not being set

--- src/main/groovy/com/acuo/rules/services/EligibilityService.groovy
+++ src/main/groovy/com/acuo/rules/services/EligibilityService.groovy
@@ -1,10 +1,17 @@
 package com.acuo.rules.services
 
 import org.kie.api.runtime.KieSession
+import org.slf4j.LoggerFactory
 
 class EligibilityService extends RulesServiceImpl {
 
-    protected KieSession newSession() {
-        return engine.newKieSession("EligibilityKS")
+    final def ksessionName = "EligibilityKS"
+
+    final def ruleLogger = LoggerFactory.getLogger(ksessionName)
+
+    KieSession newSession() {
+        def session = engine.newKieSession(ksessionName)
+        session.setGlobal("log", ruleLogger)
+        return session
     }
 }
